6855f483265f771bd6a5f155f0b525c72ba2ea51
Thiago Andre <devthiart@gmail.com>
2024-02-07 16:27:08 -0300

    Formating tags in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-    <CarrinhoProvider>
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/carrinho" element={ <Carrinho /> } />
-        <Route path="*" element={ <PaginaErro /> } />
-      </Routes>
-    </CarrinhoProvider>
+      <CarrinhoProvider>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/carrinho" element={ <Carrinho /> } />
+          <Route path="*" element={ <PaginaErro /> } />
+        </Routes>
+      </CarrinhoProvider>
     </BrowserRouter>
   );
 }
